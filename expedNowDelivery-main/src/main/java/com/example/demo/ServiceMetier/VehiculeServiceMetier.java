fbfea60dccb33dbe7a58ab945b24fd1610101faf
asma-ben-kh <asmaasma.benkhalifa@gmail.com>
2025-06-12 15:52:02 +0000

    commit

--- expedNowDelivery-main/src/main/java/com/example/demo/ServiceMetier/VehiculeServiceMetier.java
+++ expedNowDelivery-main/src/main/java/com/example/demo/ServiceMetier/VehiculeServiceMetier.java
@@ -1,7 +1,11 @@
 package com.example.demo.ServiceMetier;
+import com.example.demo.ModelDomain.Vehicule;
 
 public interface VehiculeServiceMetier {
 
-    
+    public Vehicule saveVoiture(Vehicule vehicule);
+    public Void deleteVehicule(Long id);
+    public Vehicule updateVehicule(Vehicule vehicule);
+    public void assignerVehicule(Long id, long livreurId);
 
 }
--- expedNowDelivery-main/src/main/java/com/example/demo/ServiceMetier/VehiculeServiceMetierImp.java
+++ expedNowDelivery-main/src/main/java/com/example/demo/ServiceMetier/VehiculeServiceMetierImp.java
@@ -1,5 +1,34 @@
 package com.example.demo.ServiceMetier;
+import com.example.demo.ModelDomain.Vehicule;
+import com.example.demo.ServiceMetier.VehiculeServiceMetier;
 
-public class VehiculeServiceMetierImp {
+public class VehiculeServiceMetierImp  implements VehiculeServiceMetier{
+
+    @Override
+    public void assignerVehicule(Long id, long livreurId) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Void deleteVehicule(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Vehicule saveVoiture(Vehicule vehicule) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Vehicule updateVehicule(Vehicule vehicule) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    
 
 }
